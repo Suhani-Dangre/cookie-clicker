c3fe193c4939b16ab8e1aeceb7011ae1754efa33
Suhani Dangre <suhani.dangre@gmail.com>
2020-09-22 08:48:24 -0600

    add cookie clicker website

--- mad-libs.js
+++ mad-libs.js
@@ -60,8 +60,17 @@ console.log('sport', sport)
 let sportEl = document.getElementById(sport)
 
 //Final Story
-Adj1El.innerHTML=`Pizza was invented by a ${Adj1} ${nationality1} chef named ${person1}.
- To make pizza, you need to take a lump of ${noun1}, and make a thin, round ${Adj2} ${noun2}.
- Then you cover it with ${Adj3} sauce, ${Adj4} cheese, and fresh chopped ${nounP1}.
- When it is done, cut it into ${number} ${shape}. Some kids like ${food} pizza. Many people
- after a long day of ${sport}. If I could, I would eat pizza ${number2} times a day!`
+Adj1El.innerHTML=`Pizza was invented by a 
+${Adj1}
+ ${nationality1} chef named ${person1}.
+ To make pizza, you need to take a lump 
+ of ${noun1}, and make a thin, round ${Adj2}
+  ${noun2}.
+ Then you cover it with ${Adj3} sauce,
+  ${Adj4} cheese, and fresh chopped
+   ${nounP1}.
+ When it is done, cut it into ${number}
+  ${shape}. Some kids like ${food} pizza.
+   Many people
+ after a long day of ${sport}. If I could,
+  I would eat pizza ${number2} times a day!`
